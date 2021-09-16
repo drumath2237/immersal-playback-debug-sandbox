a44a0f2d2a126e48881d41aa42cea36525852bbe
drumath <2237k.drumath@gmail.com>
2021-09-16 21:45:53 +0900

    :sparkles: 余計なplane detectionやらログ表示やらを削除

--- Assets/Scenes/ARCore/ARCoreSessionRecorder.cs
+++ Assets/Scenes/ARCore/ARCoreSessionRecorder.cs
@@ -9,47 +9,47 @@ using UnityEngine.XR.ARCore;
 
 namespace UnityEngine.XR.ARFoundation.Samples
 {
-    class LogUtil
-    {
-        List<string> m_Entries = new List<string>();
-
-        string m_FullLog = "";
-
-        int m_Count;
-
-        bool m_Dirty;
-
-        public int maxCount { get; set; } = 10;
-
-        public void Log(string msg)
-        {
-            m_Entries.Add($"{m_Count}> {msg}");
-            m_Count++;
-            m_Dirty = true;
-        }
-
-        public override string ToString()
-        {
-            if (m_Dirty)
-            {
-                var removeCount = m_Entries.Count - maxCount;
-                if (removeCount > 0)
-                {
-                    m_Entries.RemoveRange(0, removeCount);
-                }
-
-                m_FullLog = string.Join("\n", m_Entries);
-                m_Dirty = false;
-            }
-
-            return m_FullLog;
-        }
-    }
+    // class LogUtil
+    // {
+    //     List<string> m_Entries = new List<string>();
+    //
+    //     string m_FullLog = "";
+    //
+    //     int m_Count;
+    //
+    //     bool m_Dirty;
+    //
+    //     public int maxCount { get; set; } = 10;
+    //
+    //     public void Log(string msg)
+    //     {
+    //         m_Entries.Add($"{m_Count}> {msg}");
+    //         m_Count++;
+    //         m_Dirty = true;
+    //     }
+    //
+    //     public override string ToString()
+    //     {
+    //         if (m_Dirty)
+    //         {
+    //             var removeCount = m_Entries.Count - maxCount;
+    //             if (removeCount > 0)
+    //             {
+    //                 m_Entries.RemoveRange(0, removeCount);
+    //             }
+    //
+    //             m_FullLog = string.Join("\n", m_Entries);
+    //             m_Dirty = false;
+    //         }
+    //
+    //         return m_FullLog;
+    //     }
+    // }
 
     [RequireComponent(typeof(ARSession))]
     public class ARCoreSessionRecorder : MonoBehaviour
     {
-        LogUtil m_Log = new LogUtil();
+        // LogUtil m_Log = new LogUtil();
 
         ARSession m_Session;
 
@@ -83,7 +83,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
         void Log(string msg)
         {
             Debug.Log(msg);
-            m_Log.Log(msg);
+            // m_Log.Log(msg);
         }
 
         static string GetFileSize(string path)
@@ -129,13 +129,11 @@ namespace UnityEngine.XR.ARFoundation.Samples
                 {
                     if (playbackStatus != ArPlaybackStatus.Finished && GUILayout.Button("Start recording"))
                     {
-                        using (var config = new ArRecordingConfig(session))
-                        {
-                            config.SetMp4DatasetFilePath(session, m_Mp4Path);
-                            config.SetRecordingRotation(session, GetRotation());
-                            var status = subsystem.StartRecording(config);
-                            Log($"StartRecording to {config.GetMp4DatasetFilePath(session)} => {status}");
-                        }
+                        using var config = new ArRecordingConfig(session);
+                        config.SetMp4DatasetFilePath(session, m_Mp4Path);
+                        config.SetRecordingRotation(session, GetRotation());
+                        var status = subsystem.StartRecording(config);
+                        Log($"StartRecording to {config.GetMp4DatasetFilePath(session)} => {status}");
                     }
 
                     if (File.Exists(m_Mp4Path) && GUILayout.Button("Start playback"))
@@ -189,7 +187,7 @@ namespace UnityEngine.XR.ARFoundation.Samples
                 GUILayout.Label(m_PlaybackStatusMessage);
                 GUILayout.Label(m_RecordingStatusMessage);
                 GUILayout.Space(50);
-                GUILayout.Label(m_Log.ToString());
+                // GUILayout.Label(m_Log.ToString());
             }
             else
             {
